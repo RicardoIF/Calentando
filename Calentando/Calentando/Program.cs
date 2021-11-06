57d9084a4451cbd8e08e9d3cb526d4abeb9fa647
RicardoIF <endy5039@gmail.com>
2021-11-05 23:16:29 -0400

    Calentando el brazo

---
+++ Calentando/Calentando/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Calentando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "";
+            Console.Write("Escriba una horacion: ");
+            x = Console.ReadLine();
+
+            string[] texto = x.Split(' ');
+            string palabra;
+            string signos = " ";
+            bool pass = false;
+            
+            for (int i = 0; i < texto.Length ; i++)
+            {
+                signos = "";
+                palabra = texto[i];
+                for (int j = palabra.Length - 1; j >= 0 ; j--)
+                {
+                    if (palabra[j] == '.' || palabra[j] == ',' || palabra[j] == ';' || palabra[j] == ':')
+                    {
+                        signos = Convert.ToString(palabra[j]);
+                        pass = true;
+                    }
+                    else
+                    {
+                        Console.Write(palabra[j]);
+                    }                    
+                }
+                if (pass)
+                {
+                    Console.Write(signos);
+                }
+                Console.Write(" ");
+            }
+
+        }
+    }
+}
